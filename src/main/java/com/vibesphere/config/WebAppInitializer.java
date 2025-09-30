1f658447646d65cfda374189e793478db9cb67cc
mdmasiuddin-55 <mdmasiuddin55@gmail.com>
2025-09-30 14:52:58 +0530

    compilation-failure-fixes

--- src/main/java/com/vibesphere/config/WebAppInitializer.java
+++ src/main/java/com/vibesphere/config/WebAppInitializer.java
@@ -19,12 +19,24 @@
 //         return new String[] { "/" };
 //     }
 // }
-@Override
-protected Class<?>[] getRootConfigClasses() {
-    return null;
-}
+package com.vibesphere.config;
 
-@Override
-protected Class<?>[] getServletConfigClasses() {
-    return new Class[] { AppConfig.class };
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 }
